14873fb067925e085c126a571fb55a3a9be75da3
Tijan-Tomislav Radoš <tr46286@fer.hr>
2018-06-19 20:44:39 +0200

    1

--- UniprotDistributedServer/Program.cs
+++ UniprotDistributedServer/Program.cs
@@ -36,11 +36,11 @@ namespace UniprotDistributedServer
             BaseDataAccess DataBase = new BaseDataAccess("Data Source=storage.bioinfo.pbf.hr,8758;Initial Catalog=configuration;Integrated Security=False;User Id=tijan;Password=tijan99;MultipleActiveResultSets=True");
             string MediationDirectory, SourceFile;
 
-            using (DataSet ConfigData = DataBase.ExecuteFillDataSet("select * from slaves", null))
-            {
-                MediationDirectory = ConfigData.Tables[0].Select("is_master = 1")[0]["load_mediation_directory"].ToString();
-                SourceFile = ConfigData.Tables[0].Select("is_master = 1")[0]["load_source_file"].ToString();
-            }
+            //using (DataSet ConfigData = DataBase.ExecuteFillDataSet("select * from slaves", null))
+            //{
+            //    MediationDirectory = ConfigData.Tables[0].Select("is_master = 1")[0]["load_mediation_directory"].ToString();
+            //    SourceFile = ConfigData.Tables[0].Select("is_master = 1")[0]["load_source_file"].ToString();
+            //}
 
             #endregion
         }
