f8553a7d96b071a54ab64cb410d5fb7630a5db8e
Tijan-Tomislav Radoš <tr46286@fer.hr>
2018-06-21 01:02:26 +0200

    arguments changed

--- UniprotDistributedSlave/Program.cs
+++ UniprotDistributedSlave/Program.cs
@@ -14,8 +14,20 @@ namespace UniprotDistributedSlave
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
 
-            BuildWebHost(args).Run();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(configuration)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+            //BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
