75ebc8b1f66c579e00d6badb9dd52bd910051c57
Tijan-Tomislav Radoš <tr46286@fer.hr>
2018-06-26 04:40:13 +0200

    2

--- UniprotDistributedServer/Program.cs
+++ UniprotDistributedServer/Program.cs
@@ -40,7 +40,7 @@ namespace UniprotDistributedServer
             //Configuration is set up on DefaultConnection string in this case
             BaseDataAccess DataBase = new BaseDataAccess("Data Source=localhost,8758;Initial Catalog=configuration;Integrated Security=False;User Id=tijan;Password=tijan99;MultipleActiveResultSets=True");
 
-            using (DataSet ConfigData = DataBase.ExecuteFillDataSet("select slave_id, concat('Data Source=localhost,', db_port, ';Initial Catalog=', database_name, ';Integrated Security=False;User Id=', username, ';Password=', password, ';MultipleActiveResultSets=True') as database_connection_string, concat(server_name, ':', api_port) as api_call, api_port, server_level, working_directory, main_table  FROM slaves WHERE is_using = 'true';", null))
+            using (DataSet ConfigData = DataBase.ExecuteFillDataSet("select slave_id, concat('Data Source=localhost,', db_port, ';Initial Catalog=', database_name, ';Integrated Security=False;User Id=', username, ';Password=', password, ';MultipleActiveResultSets=True') as database_connection_string, concat('http://', server_name, ':', api_port) as api_call, api_port, server_level, working_directory, main_table  FROM slaves WHERE is_using = 'true';", null))
             {
                 foreach(DataRow row in ConfigData.Tables[0].Rows)
                 {
