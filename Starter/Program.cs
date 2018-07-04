fbd4684ea9192638b8a14753490d09e309cb109f
Tijan-Tomislav Radoš <tr46286@fer.hr>
2018-07-04 20:49:15 +0200

    6

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
+using System.Security.Principal;
 using System.Text;
 
 namespace Starter
@@ -110,7 +111,13 @@ namespace Starter
                     Byte[] info = new UTF8Encoding(true).GetBytes(script);
                     // Add some information to the file.
                     fs.Write(info, 0, info.Length);
+
+                    FileAttributes attributes = File.GetAttributes(path);
+
+                    File.SetAttributes(path, attributes | FileAttributes.Normal);
                 }
+
+
             }
 
             catch (Exception ex)
