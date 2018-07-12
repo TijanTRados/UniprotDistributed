bca027aa85ea6a9d9458ef88aceba39f792890b1
Tijan-Tomislav Radoš <tr46286@fer.hr>
2018-07-12 03:28:47 +0200

    yyy

--- UniprotDistributedSlave/Controllers/ValuesController.cs
+++ UniprotDistributedSlave/Controllers/ValuesController.cs
@@ -63,6 +63,7 @@ namespace UniprotDistributedSlave.Controllers
                     //fileLines.RemoveRange(0, 4);
                     //fileLines.RemoveRange(fileLines.Count - 3, 2);
                     Console.WriteLine("Trying to write text to " + Program.myWorkingDirectory + Request.Headers["file-name"]);
+                    Console.WriteLine("Text starting with: " + fileLines[0] + "...");
 
                     System.IO.File.WriteAllText(Program.myWorkingDirectory + Request.Headers["file-name"], string.Join('\n', fileLines));
 
